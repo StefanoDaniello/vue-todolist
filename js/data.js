db42c512e3734cd3cd24ef105c3f2f605c761066
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-02 15:17:09 +0200

    start

---
+++ js/data.js
@@ -0,0 +1,23 @@
+export const todo = [
+    {
+        id: 1,
+        text: 'Costume da bagno',
+        done: false
+    },
+    {
+        id: 2,
+        text: 'Carta di credito',
+        done: false
+    },
+    {
+        id: 3,
+        text: 'Paletta e secchiello',
+        done: false
+    },
+    {
+        id: 4,
+        text: 'Crema solare',
+        done: false
+    }
+    
+] 
